43e2cb5f23e997fc5ed2bb44a580fc10ee739c8c
fede <federico.levatti@hotmail.com>
2021-12-22 13:38:13 -0300

    grid

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -34,8 +34,8 @@ module.exports = {
     {
       '/LenguajeCss/' : [
         '' ,
-        'flexbox' ,
-        'grid'
+        'Grid' ,
+        'flexbox'
      ] ,
       '/Javascript/' : [
         '' ,
